b8fdf2fc9705abcc904afa20942017be889dc1aa
Nico Stern <s92nster@uni-bonn.de>
2023-08-09 00:00:30 +0200

    restabilisiert

--- CS50quiz/Views/ContentView.swift
+++ CS50quiz/Views/ContentView.swift
@@ -1,80 +1,94 @@
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()  // Instantiate ViewModel
     @State private var startQuiz = false
     @State private var selectedQuestionCount = 1 // Default value
     @State private var symbolColor: Color = .black
-
+    @State private var motionManager = CMMotionManager()
     
     private func changeSymbolColor() {
-    // Generiere zufällige RGB-Farbwerte
-    let randomRed = Double.random(in: 0...1)
-    let randomGreen = Double.random(in: 0...1)
-    let randomBlue = Double.random(in: 0...1)
+        let randomRed = Double.random(in: 0...1)
+        let randomGreen = Double.random(in: 0...1)
+        let randomBlue = Double.random(in: 0...1)
+        symbolColor = Color(red: randomRed, green: randomGreen, blue: randomBlue)
+    }
     
-    // Setze die generierte Farbe als neue Farbe für das Symbol
-    symbolColor = Color(red: randomRed, green: randomGreen, blue: randomBlue)
-}
-
+    private func startShakeDetection() {
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: .main) { accelerometerData, error in
+            guard let data = accelerometerData else { return }
+            let acceleration = data.acceleration
+            
+            // Hier kannst du die Schüttelbewegung erkennen
+            if abs(acceleration.x) > 1.5 || abs(acceleration.y) > 1.5 || abs(acceleration.z) > 1.5 {
+                changeSymbolColor()
+            }
+        }
+    }
     
     var body: some View {
-        NavigationView {
-            VStack {                
-                Spacer()
-                
-                Image(systemName: "graduationcap.fill")
-                    .font(.system(size: 100))
-                    .foregroundColor(symbolColor) // Verwende die symbolColor
-                    .padding(.bottom, 40)
+    NavigationView {
+        VStack {
+            Spacer()
+            
+            Image(systemName: "graduationcap.fill")
+                .font(.system(size: 100))
+                .foregroundColor(symbolColor)
+                .padding(.bottom, 40)
 
-                
-                Text("This is the CS50 Quiz!")
-                    .font(.title)
-                    .padding(.bottom, 5)
-                
-                Text("by @nico_strn")
-                    .padding(.bottom, 60)
-                
-                NavigationLink(
-                    destination: QuizView(selectedQuestionCount: selectedQuestionCount, viewModel: viewModel),  // Pass ViewModel instance
-                    isActive: $startQuiz,
-                    label: {
-                        EmptyView()
-                    }
-                )
-                
-                Picker("How many questions?:", selection: $selectedQuestionCount) {
-                    ForEach(viewModel.questionCounts, id: \.self) { count in
-                        Text("\(count) Question\(count == 1 ? "" : "s")")
-                    }
+            
+            Text("This is the CS50 Quiz!")
+                .font(.title)
+                .padding(.bottom, 5)
+            
+            Text("by @nico_strn")
+                .padding(.bottom, 60)
+            
+            NavigationLink(
+                destination: QuizView(selectedQuestionCount: selectedQuestionCount, viewModel: viewModel),
+                isActive: $startQuiz,
+                label: {
+                    EmptyView()
                 }
+            )
+            
+            Picker("How many questions?:", selection: $selectedQuestionCount) {
+                ForEach(viewModel.questionCounts, id: \.self) { count in
+                    Text("\(count) Question\(count == 1 ? "" : "s")")
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                changeSymbolColor()
+            }, label: {
+                Text("Shake it!")
+                    .font(.headline)
+                    .foregroundColor(dblue)
+            })
+            .padding(.bottom, 10)
+            
+            PrimaryButton(text: "Start")
+                .onTapGesture {
+                    startQuiz = true
+                }
+                .padding(.bottom, 40)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(red: 220/255, green: 237/255, blue: 255/255)) // pastel baby blue
+        .onAppear {
+            viewModel.getData()
+            startShakeDetection()
+        }
+        .onDisappear {
+            motionManager.stopAccelerometerUpdates()
+        }
 
-                .pickerStyle(WheelPickerStyle()) // Use the WheelPickerStyle
-                
-                Spacer()
-                
-                Button(action: {
-                    changeSymbolColor()
-                }, label: {
-                    Text("Cooolor")
-                        .font(.headline)
-                        .foregroundColor(dblue)
-                })
-                .padding(.bottom, 10)
-                
-                PrimaryButton(text: "Start")
-                    .onTapGesture {
-                        startQuiz = true
-                    }
-                    .padding(.bottom, 40)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .edgesIgnoringSafeArea(.all)
-            .background(Color(red: 220/255, green: 237/255, blue: 255/255)) // pastel baby blue
-            .onAppear {
-                viewModel.getData()
-            }
         }
     }
     
